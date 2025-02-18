e53847642fd465b8bb5951f928cb2d1baf9fa77a
Samuel Song <samuelsong79@gmail.com>
2025-02-18 10:51:27 -0600

    Auto commit from pre-commit hook

--- CS 1337/Assignment2/ArraySort.cpp
+++ CS 1337/Assignment2/ArraySort.cpp
@@ -21,8 +21,48 @@ int main()
     ifstream inputfile ("arrayData.txt");
 
     int dataArray[5];
-    int pointerArray[5];
+    int *pointerArray[5];
 
-    
+    unsigned maxElements;
+
+    while (ReadIntoDataArray(inputfile,maxElements,dataArray,pointerArray))
+    {
+        Sorting(pointerArray,maxElements);
+        cout<<"Now displaying data in sorted order";
+        DisplayArray(pointerArray,maxElements);
+        cout<<"Now displaying data in the original order";
+        DisplayArray(dataArray,maxElements);
+        cout<<"Press the enter key once or twice to continue..."<<endl;cin.ignore();cin.get();
+    }
+}
+
+
+bool ReadIntoDataArray(ifstream& inputFileStreamObj,unsigned& maxElements,int dataArray,int* pointerArray)
+{
+    for (unsigned i=0;i<maxElements;i++) 
+    {
+        inputFileStreamObj>>dataArray[i];
+        pointerArray[i]=&dataArray[i];
+    }
+    return true;
+}
+
+void DisplayArray(int dataArray,unsigned maxElements)
+{
+
+}
+
+void DisplayArray(int* pointerArray,unsigned maxElements)
+{
+
+}
+
+void Sorting(int* pointerArray,unsigned maxElements)
+{
+
+}
+
+void SwapIntPtr(int** a,int** b)
+{
 
 }
