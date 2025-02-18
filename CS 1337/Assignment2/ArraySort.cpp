db87887167d08f088fe31d18d219d3a7717155a9
Samuel Song <samuelsong79@gmail.com>
2025-02-18 09:08:30 -0600

    commit

---
+++ CS 1337/Assignment2/ArraySort.cpp
@@ -0,0 +1,22 @@
+/*
+Program Name: Array Sort Using Pointers
+Date: 2/18/2025
+Author: Samuel Song
+Module Purpose: 
+*/
+
+#include <iostream>
+
+using namespace std;
+
+bool ReadIntoDataArray(ifstream& inputFileStreamObj, unsigned& maxElements, int dataArray[], int* pointerArray[]);
+void DisplayArray(int dataArray[], unsigned maxElements);
+void DisplayArray(int* pointerArray[],unsigned maxElements);
+void Sorting(int* pointerArray[],unsigned maxElements);
+void SwapIntPtr(int** a,int** b);
+
+int main()
+{
+
+    
+}
